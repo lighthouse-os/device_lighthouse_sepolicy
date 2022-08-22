92f8b30ac4d460151c906331d9b037099ccbab81
CannedShroud <hypersync112@gmail.com>
2022-08-22 16:25:09 +0530

    sepolicy: Adapt

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,20 +16,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/common/public
+    device/lighthouse/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/common/private
+    device/lighthouse/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/common/dynamic \
-    device/evolution/sepolicy/common/system
+    device/lighthouse/sepolicy/common/dynamic \
+    device/lighthouse/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/common/dynamic \
-    device/evolution/sepolicy/common/vendor
+    device/lighthouse/sepolicy/common/dynamic \
+    device/lighthouse/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/evolution/sepolicy/legacy-common/sepolicy.mk
+-include device/lighthouse/sepolicy/legacy-common/sepolicy.mk
--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/exynos/private
+    device/lighthouse/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/exynos/dynamic \
-    device/evolution/sepolicy/exynos/system
+    device/lighthouse/sepolicy/exynos/dynamic \
+    device/lighthouse/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/evolution/sepolicy/exynos/dynamic \
-    device/evolution/sepolicy/exynos/vendor
+    device/lighthouse/sepolicy/exynos/dynamic \
+    device/lighthouse/sepolicy/exynos/vendor
 endif
